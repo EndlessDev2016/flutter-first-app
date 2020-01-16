081f1d9fdac47f54d20ce21b2a346619428e043a
lee-gram-pc <nanohana2016s@gmail.com>
2020-01-16 19:30:03 +0900

    adding file

--- lib/main.dart
+++ lib/main.dart
@@ -6,51 +6,48 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return MyAppState();
-  }
-}
-
-class MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   var questionIndex = 0;
-
+    
   void answerQuestion() {
-    setState(() {
-      questionIndex = questionIndex + 1;
-    });
+    questionIndex += 1;
     print(questionIndex);
+    print('Answer choson 1');
   }
 
   @override
   Widget build(BuildContext context) {
     var questions = [
-      'What\'s your favorite color?',
-      'What\'s your favorite animarl?',
+      'What\s your favorite color?',
+      'What\s your favorite animal?',
     ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('My First App'),
+          title: Text('MY First App'),
         ),
         body: Column(
           children: [
             Text(questions[questionIndex]),
             RaisedButton(
-              child: Text('Answer 1'),
+              child: Text(
+                'Answer 1',
+              ),
               onPressed: answerQuestion,
             ),
             RaisedButton(
-              child: Text('Answer 2'),
-              onPressed: () => print('Answer 2 chosen!'),
+              child: Text(
+                'Answer 1',
+              ),
+              onPressed: () => print('answer choson 2'),
             ),
             RaisedButton(
-              child: Text('Answer 3'),
+              child: Text(
+                'Answer 1',
+              ),
               onPressed: () {
-                // ...
-                print('Answer 3 chosen');
+                print('answer choson 3');
               },
             ),
           ],
